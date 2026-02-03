ddcf525b8a2fddb1f4c191f6685f8ac4db1c3f3f
Raf <github.com.chair662@passmail.net>
2026-02-03 15:02:29 +0100

    Fix critical and high severity CVEs

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"testing"
 
-	"github.com/jetstack/cert-manager/test/acme/dns"
+	acmetest "github.com/cert-manager/cert-manager/test/acme"
 )
 
 var (
@@ -16,12 +16,12 @@ func TestRunsSuite(t *testing.T) {
 	// snippet of valid configuration that should be included on the
 	// ChallengeRequest passed as part of the test cases.
 
-	fixture := dns.NewFixture(&ns1DNSProviderSolver{},
-		dns.SetResolvedZone(zone),
-		dns.SetAllowAmbientCredentials(false),
-		dns.SetManifestPath("testdata/ns1"),
-		dns.SetBinariesPath("_out/kubebuilder/bin"),
+	fixture := acmetest.NewFixture(&ns1DNSProviderSolver{},
+		acmetest.SetResolvedZone(zone),
+		acmetest.SetAllowAmbientCredentials(false),
+		acmetest.SetManifestPath("testdata/ns1"),
 	)
 
-	fixture.RunConformance(t)
+	fixture.RunBasic(t)
+	fixture.RunExtended(t)
 }
